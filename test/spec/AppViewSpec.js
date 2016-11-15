07b7da21799b4b69b936b8fcd82cbeb85b6a37bb
niwang55 <nickwang55@gmail.com>
2016-11-15 15:19:50 -0800

    added specs for SongQueueView.js

--- test/spec/AppViewSpec.js
+++ test/spec/AppViewSpec.js
@@ -23,6 +23,15 @@ describe('AppView', function() {
     expect(appView.playerView).to.be.an.instanceof(PlayerView);
   });
 
+  it('should generate a LibraryView when created', function() {
+    expect(appView.libraryView).to.be.an.instanceof(LibraryView);
+  });
+
+  it('should generate a SongQueueView when created', function() {
+    expect(appView.songQueueView).to.be.an.instanceof(SongQueueView);
+  });
+
+
   describe('when the currently playing song changes', function() {
     it('updates current song in playerView', function() {
       var song = app.get('library').at(0);
